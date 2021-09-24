ad993d0d4f2044946604a490611c93f3a89b052d
victoryang00 <yangyw@shanghaitech.edu.cn>
2021-09-24 12:47:20 +0800

    add dependency

--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
-use std::process::Command;
+use std::{env, path::Path, process::Command};
 
 fn main() {
-    Command::new("./ior/bootstrap").status().unwrap();
+    println!("cargo:rerun-if-changed=./ior");
+    let root = Path::new("./ior");
+    env::set_current_dir(&root).is_ok();
+    Command::new("git").args(&["submodule","init"]).status().unwrap();
+    Command::new("git").args(&["submodule","update"]).status().unwrap();
+    Command::new("./bootstrap").status().unwrap();
     // Note that there are a number of downsides to this approach, the comments
     // below detail how to improve the portability of these commands.
-    Command::new("./ior/configure")
+    Command::new("./configure")
         .args(&["CFLAGS=\"-g -O0 -fPIC\""])
         .status()
         .unwrap();
