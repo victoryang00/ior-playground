79d59d6180bfcd600c31199304dd16ad8b0a06c1
victoryang00 <yangyw@shanghaitech.edu.cn>
2021-09-24 12:31:27 +0800

    add new

--- build.rs
+++ build.rs
@@ -1,4 +1,14 @@
+use std::process::Command;
+
 fn main() {
+    Command::new("./ior/bootstrap").status().unwrap();
+    // Note that there are a number of downsides to this approach, the comments
+    // below detail how to improve the portability of these commands.
+    Command::new("./ior/configure")
+        .args(&["CFLAGS=\"-g -O0 -fPIC\""])
+        .status()
+        .unwrap();
+    Command::new("make").args(&["install"]).status().unwrap();
     println!("cargo:rustc-link-lib=aiori");
     println!("cargo:rustc-link-lib=mpi");
 }
