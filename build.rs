80efb282b163bd584e1fa58a6c3fc222c58596c2
victoryang00 <40686366+victoryang00@users.noreply.github.com>
2021-09-24 12:58:58 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -4,16 +4,16 @@ fn main() {
     println!("cargo:rerun-if-changed=./ior");
     let root = Path::new("./ior");
     env::set_current_dir(&root).is_ok();
-    Command::new("git").args(&["submodule","init"]).status().unwrap();
-    Command::new("git").args(&["submodule","update"]).status().unwrap();
-    Command::new("./bootstrap").status().unwrap();
+    Command::new("git").args(&["submodule","init"]).output().unwrap();
+    Command::new("git").args(&["submodule","update"]).output().unwrap();
+    Command::new("./bootstrap").output().unwrap();
     // Note that there are a number of downsides to this approach, the comments
     // below detail how to improve the portability of these commands.
     Command::new("./configure")
         .args(&["CFLAGS=\"-g -O0 -fPIC\""])
-        .status()
+        .output()
         .unwrap();
-    Command::new("make").args(&["install"]).status().unwrap();
+    Command::new("make").args(&["install"]).output().unwrap();
     println!("cargo:rustc-link-lib=aiori");
     println!("cargo:rustc-link-lib=mpi");
 }
